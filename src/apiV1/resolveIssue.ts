a20a09dd5fc6c7d75b6c1e9ed1959da260b43e2d
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-21 01:25:25 +0200

    fix: use the issue id from the url params in the resolveIssue API handler

--- src/apiV1/resolveIssue.ts
+++ src/apiV1/resolveIssue.ts
@@ -5,9 +5,12 @@ import { knex } from "../db";
 import { AgentResolveIssueResponse } from "./validators";
 
 export const resolveIssue: Middleware = async (ctx) => {
+  // id is guaranteed to be a number by the Joi params validator
+  const { id } = (ctx.request.params as unknown) as { id: number };
+
   const [issue]: AgentResolveIssueResponse[] = await knex("issues")
     .update({ status: "resolved" }, "*")
-    .where({ id: 3, status: "assigned" });
+    .where({ id, status: "assigned" });
 
   if (!issue) {
     ctx.status = 404;
