839b7109623a2bd3b37d5344d68fa6314736fe6e
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-21 13:10:53 +0200

    test: add POST /apiv1/issues/:id/resolve test

--- src/apiV1/resolveIssue.ts
+++ src/apiV1/resolveIssue.ts
@@ -14,6 +14,7 @@ const resolveAndAssignNext = async (id: number) => {
       return;
     }
     resolvedIssue = updatedIssue;
+    resolvedIssue.nextAssignedIssueId = null;
 
     const nextUnassignedIssue = await trx("issues")
       .select("id")
@@ -21,7 +22,7 @@ const resolveAndAssignNext = async (id: number) => {
         status: "new",
       })
       .orderBy("id", "asc")
-      .first("id");
+      .first();
 
     if (nextUnassignedIssue?.id) {
       const nextUnassignedIssueId = nextUnassignedIssue.id;
