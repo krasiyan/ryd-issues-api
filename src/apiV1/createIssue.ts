d4a7737b5becd9859c81a7dee3946530924d96db
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-21 02:13:53 +0200

    feat: implement the agent issue assignment logic in the createIssue API handler

--- src/apiV1/createIssue.ts
+++ src/apiV1/createIssue.ts
@@ -2,16 +2,51 @@ import { Middleware } from "koa";
 
 import { knex } from "../db";
 
-import { NewIssueRequest, Issue } from "./validators";
+import { NewIssueRequest, Issue, Agent } from "./validators";
+
+const getRandomAgent = (agents: Agent[]): Agent =>
+  agents[Math.floor(Math.random() * agents.length)];
 
 export const createIssue: Middleware = async (ctx) => {
-  const issue = <NewIssueRequest>ctx.request.body;
+  const reqData = <NewIssueRequest>ctx.request.body;
 
-  const [dbIssue]: Issue[] = await knex
-    .insert({ ...issue, status: "new" })
-    .into("issues")
-    .returning("*");
+  try {
+    await knex.transaction(async (trx) => {
+      const availableAgents: Agent[] = await trx("agents")
+        .select("*")
+        .from("agents")
+        .whereNotExists(
+          trx
+            .from("issues")
+            .select("id")
+            .whereRaw(
+              "agents.id = issues.\"agentId\" AND issues.status = 'assigned'"
+            )
+        );
 
-  // TODO: assign issue to agent (if available)
-  ctx.body = dbIssue;
+      const issue: Omit<Issue, "id"> = {
+        ...reqData,
+        status: "new",
+      };
+
+      if (availableAgents.length > 0) {
+        const randomAgent = getRandomAgent(availableAgents);
+        issue.status = "assigned";
+        issue.agentId = randomAgent.id;
+        issue.agentName = randomAgent.name;
+      }
+      const [createdIssue]: Issue[] = await trx
+        .insert(issue)
+        .into("issues")
+        .returning("*");
+
+      ctx.body = createdIssue;
+      if (!ctx.body) {
+        ctx.status = 404;
+      }
+    });
+  } catch (err) {
+    console.error("createIssue DB transaction error", err);
+    ctx.status = 500;
+  }
 };
