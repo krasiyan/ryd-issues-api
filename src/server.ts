4275b407a21cd6437baf57fc651968c2935efa5a
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-21 03:35:11 +0200

    feat: use the koa-logger to log all requests and responses

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import koa from "koa";
+import koaLogger from "koa-logger";
 
 import { apiV1Router } from "./apiV1";
 
 const app = new koa();
+app.use(koaLogger());
 app.use(apiV1Router.middleware());
 
 export { app };
