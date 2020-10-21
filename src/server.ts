22ab420a04b2a5f6dc89fe8c64a0709b9ad2949f
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-21 03:45:45 +0200

    feat: global error middleware outputting validation errors (i.e. from Joi) as JSON in the res. body
    - extend README.md

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,40 @@
-import koa from "koa";
+import koa, { Middleware } from "koa";
 import koaLogger from "koa-logger";
 
 import { apiV1Router } from "./apiV1";
 
+const errorMiddleware: Middleware = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    // handle Joi validation error
+    if (err.isJoi) {
+      ctx.status = err.statusCode;
+      ctx.body = {
+        name: err.name,
+        details: err.details,
+        msg: err.msg,
+      };
+      return;
+    }
+
+    // handle misc bad request errors (i.e. invalid JSON req. body)
+    if (err?.statusCode === 400 && err.msg) {
+      ctx.status = err.statusCode;
+      ctx.body = {
+        msg: err.msg,
+      };
+      return;
+    }
+
+    console.error("unexpected API error", err);
+    ctx.status = 500;
+  }
+};
+
 const app = new koa();
 app.use(koaLogger());
+app.use(errorMiddleware);
 app.use(apiV1Router.middleware());
 
 export { app };
